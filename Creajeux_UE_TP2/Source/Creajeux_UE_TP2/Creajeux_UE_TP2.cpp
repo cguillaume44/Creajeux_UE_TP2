cf3f16db441cc385561432af63590137398272cc
Corentin Guillaume <guillaume.corentin.pro@gmail.com>
2024-10-12 13:08:26 +0200

    Blank project + gitignore

---
+++ Creajeux_UE_TP2/Source/Creajeux_UE_TP2/Creajeux_UE_TP2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Creajeux_UE_TP2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Creajeux_UE_TP2, "Creajeux_UE_TP2" );
